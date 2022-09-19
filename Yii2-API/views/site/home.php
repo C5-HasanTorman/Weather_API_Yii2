7b1b74df6fa20e20752fb473de543b1cbb28d0ff
HasanTorman <torman.hassan@gmail.com>
2022-09-19 08:33:10 +0300

    install yii2 framework

---
+++ Yii2-API/views/site/home.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application'; ?>
+<div  class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-3">Weather Yii2 App</h1>
+
+
+    </div>
+
+    <div class="body-content">
+ 
+
+    </div>
+</div>
